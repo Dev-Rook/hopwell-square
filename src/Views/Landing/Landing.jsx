c5aa18054465859c0c96fa7a8dea6c26f3c930b9
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-09-11 15:25:08 -0500

    Map

--- src/Views/Landing/Landing.jsx
+++ src/Views/Landing/Landing.jsx
@@ -18,29 +18,41 @@ import Featured from "../../Components/Sliders/Featured/Featured";
 import Contractor from "../../Components/Sliders/Contactors/Contractor";
 import Business from "../../Components/Sliders/Business/Business";
 
-
 const Landing = () => {
-  const [loacationData, setLocationData] = useState({})
+  const [loacationData, setLocationData] = useState({});
 
-  const url = `dogwater`
+  const url = `dogwater`;
 
   const getLocationData = () => {
     axios.get(url).then((res) => {
-      setLocationData(res.data)
-    })
-  }
+      setLocationData(res.data);
+    });
+  };
 
   return (
     <div className={Styles.Landing}>
       <div className={Styles.Hero_Board}>
-      <img src={require("../../Assets/Images/RoundHill.jpeg")} alt="" className={Styles.Hero_Image} />
+        <img
+          src={require("../../Assets/Images/RoundHill.jpeg")}
+          alt=""
+          className={Styles.Hero_Image}
+        />
       </div>
 
       <div className={Styles.Title__WeatherBox}>
         <h1 className={Styles.Brand}>Hopewell Square</h1>
 
         <div className={Styles.Weather_Box}>
-
+          <iframe
+            width="100%"
+            height="100%"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?q=Lobster%20Trapp&t=&z=15&ie=UTF8&iwloc=&output=embed"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          ></iframe>
         </div>
       </div>
       <Featured />
