e46228c98c296ff779df0ac52c393bbbc21fd837
Dev Rook <110010478+Dev-Rook@users.noreply.github.com>
2022-09-11 14:02:36 -0500

    Theme

---
+++ src/Components/Sliders/Featured/Featured.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from "react";
+import Styles from "../_Styles_/SliderStyles.module.css";
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, FreeMode, Pagination, Mousewheel } from "swiper";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+// import "swiper/css/effect-fade ";
+
+import FeaturedData from "./FeaturedData";
+
+const Featured = () => {
+  const [cardData, setCardData] = useState(FeaturedData);
+
+  return (
+    <div className={Styles.Rack}>
+      <h3 className={Styles.Rack_Title}>Featured</h3>
+      <Swiper
+        speed={800}
+        modules={[Navigation, FreeMode, Mousewheel]}
+        grabCursor={true}
+        loop
+        pagination={{
+          dynamicBullets: true,
+        }}
+        mousewheel={true}
+        className={Styles.Swiper}
+        breakpoints={{
+          360: {
+            width: 360,
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          380: {
+            width: 380,
+            slidesPerView: 1.2,
+            spaceBetween: 10,
+          },
+          480: {
+            width: 480,
+            slidesPerView: 1.5,
+            spaceBetween: 10,
+          },
+          768: {
+            width: 768,
+            slidesPerView: 2,
+            spaceBetween: 10,
+          },
+          1920: {
+            width: 1920,
+            slidesPerView: 4,
+            spaceBetween: 0,
+          },
+          2000: {
+            width: 2000,
+            slidesPerView: 5,
+            spaceBetween: 0,
+          },
+        }}
+      >
+        <div className={Styles.Tray}>
+          {cardData.map((slide) => {
+            const { id, Name, Image, Category, Description } = slide;
+
+            return (
+              <SwiperSlide className={Styles.Slide}>
+                <div className={Styles.Card} key={id}>
+                  <div className={Styles.Display_Image_Container}>
+                    <img src={Image} alt="" className={Styles.Display_Image} />
+                  </div>
+
+                  <div className={Styles.Information_Box}>
+                    <div className={Styles.Name__Category}>
+                      <p className={Styles.NC_Text}>{Name}</p>
+                      <p className={Styles.NC_Text}>{Category}</p>
+                    </div>
+
+                    <span className={Styles.Description}>{Description}</span>
+
+                    <div className={Styles.Ratings__View}>
+                      <div className={Styles.Ratings_Container}>
+                        <div className={Styles.Rating}></div>
+                        <div className={Styles.Rating}></div>
+                        <div className={Styles.Rating}></div>
+                      </div>
+
+                      <button className={Styles.View_Button}>View</button>
+                    </div>
+                  </div>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </div>
+      </Swiper>
+    </div>
+  );
+};
+
+export default Featured;
--- src/Views/Landing/Landing.jsx
+++ src/Views/Landing/Landing.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import Styles from "./Styles/Landing.module.css";
 
+import Featured from "../../Components/Sliders/Featured/Featured";
 import Contractor from "../../Components/Sliders/Contactors/Contractor";
 import Business from "../../Components/Sliders/Business/Business";
 
 const Landing = () => {
   return (
     <div className={Styles.Landing}>
+      <Featured />
       <Contractor />
       <Business />
     </div>
